46effae5e2dafcf4065e1936e9184e60286fcb58
willem@leit.so <wnuke@wnuke.dev>
2020-09-29 20:24:31 -0700

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    `maven-publish`
 }
 group = "dev.wnuke"
 version = "0.0.1"
@@ -11,3 +12,15 @@ repositories {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = group as String
+                artifactId = rootProject.name
+                version = version as String
+
+                from(components["java"])
+            }
+        }
+    }
