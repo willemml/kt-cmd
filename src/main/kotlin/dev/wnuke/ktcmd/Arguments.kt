fadbce86c37053bd7cb8595590435e067be51c77
willem@leit.so <wnuke@wnuke.dev>
2020-10-03 00:42:35 -0700

    basic documentation for Argument.kt

--- src/main/kotlin/dev/wnuke/ktcmd/Arguments.kt
+++ src/main/kotlin/dev/wnuke/ktcmd/Arguments.kt
@@ -4,17 +4,34 @@ import kotlin.reflect.KClass
 
 class RuntimeCommandSyntaxError(problem: String) : RuntimeException(problem)
 
+/**
+ * Abstract argument, [T] is the type of the argument, [U] is the type of call the argument's [runs] block is called with
+ */
 abstract class Argument<T, U : Call>(
     name: String,
     val description: String,
+    /**
+     * What to run when this argument is executed
+     */
     val runs: U.(T) -> Unit,
+    /**
+     * Short name to use, ideally 1-2 characters so that it is easier to use this argument in a CLI
+     */
     val shortName: String,
+    /**
+     * Default value for this argument, usually used when the argument is optional and was not given
+     */
     val default: T,
     val type: KClass<*>
 ) {
     val prefix = "--$name"
     val shortPrefix = "-$shortName"
 
+    /**
+     * Parses [string] to get the value with type [T]
+     * @return The parsed value of [string] as [T]
+     * @throws RuntimeCommandSyntaxError when the argument value given by the user is of the wrong type
+     */
     @Throws(RuntimeCommandSyntaxError::class)
     fun parse(string: String): T {
         if (string.startsWith("$prefix=")) return parseValue(string.removePrefix("$prefix="))
@@ -22,23 +39,35 @@ abstract class Argument<T, U : Call>(
         return parseValue(string)
     }
 
+    /**
+     * @return true if [string] matches either of [prefix] and [shortPrefix] else false
+     */
     fun matches(string: String): Boolean {
         if (string.startsWith(prefix)) return true
         if (shortName.isNotEmpty() && string.startsWith(shortPrefix)) return true
         return false
     }
 
+    /**
+     * @return if [string] is equal to [prefix] or [shortPrefix]
+     */
     fun prefixOnly(string: String) = string == prefix || string == shortPrefix
 
     @Throws(RuntimeCommandSyntaxError::class)
-    abstract fun parseValue(string: String): T
+    internal abstract fun parseValue(string: String): T
 }
 
+/**
+ * Argument with type String
+ */
 class StringArgument<T : Call>(name: String, description: String = "", default: String, runs: T.(String) -> Unit, shortName: String) :
     Argument<String, T>(name, description, runs, shortName, default, String::class) {
     override fun parseValue(string: String) = string
 }
 
+/**
+ * Argument with type Int
+ */
 class IntArgument<T : Call>(name: String, description: String = "", default: Int, runs: T.(Int) -> Unit, shortName: String) :
     Argument<Int, T>(name, description, runs, shortName, default, Int::class) {
     override fun parseValue(string: String): Int {
@@ -50,6 +79,9 @@ class IntArgument<T : Call>(name: String, description: String = "", default: Int
     }
 }
 
+/**
+ * Argument with type Long
+ */
 class LongArgument<T : Call>(name: String, description: String = "", default: Long, runs: T.(Long) -> Unit, shortName: String) :
     Argument<Long, T>(name, description, runs, shortName, default, Long::class) {
     override fun parseValue(string: String): Long {
@@ -61,6 +93,9 @@ class LongArgument<T : Call>(name: String, description: String = "", default: Lo
     }
 }
 
+/**
+ * Argument with type Float
+ */
 class FloatArgument<T : Call>(name: String, description: String = "", default: Float, runs: T.(Float) -> Unit, shortName: String) :
     Argument<Float, T>(name, description, runs, shortName, default, Float::class) {
     override fun parseValue(string: String): Float {
@@ -72,6 +107,9 @@ class FloatArgument<T : Call>(name: String, description: String = "", default: F
     }
 }
 
+/**
+ * Argument with type Double
+ */
 class DoubleArgument<T : Call>(name: String, description: String = "", default: Double, runs: T.(Double) -> Unit, shortName: String) :
     Argument<Double, T>(name, description, runs, shortName, default, Double::class) {
     override fun parseValue(string: String): Double {
